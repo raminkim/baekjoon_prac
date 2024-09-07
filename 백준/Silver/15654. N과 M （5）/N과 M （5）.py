9f3e1babc12acef2bfd4ce579e55eeb451f9a537
김상수 <151427064+raminkim@users.noreply.github.com>
2024-09-07 23:40:53 +0900

    Update N과 M （5）.py

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -4,7 +4,7 @@ global n, m
 numbers = []
 lst = []
 
-def dfs(d):
+def backtracking(d):
     # d가 m과 같아지면 return
     if d == m:
         print(" ".join(map(str, lst)))
@@ -18,4 +18,4 @@ def dfs(d):
 if __name__ == "__main__":
     n, m = map(int, sys.stdin.readline().split())
     numbers = sorted(list(map(int, sys.stdin.readline().split())))
-    dfs(0)
+    backtracking(0)
