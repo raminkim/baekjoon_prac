43c888e3f5866e3d6159d2eb42a2b860d98475fb
김상수 <151427064+raminkim@users.noreply.github.com>
2025-02-17 01:18:00 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -15,20 +15,22 @@ int main() {
     string pattern;
     cin >> pattern;
 
-    string front;
-    string back;
-
     int idx = pattern.find("*");
-    front = pattern.substr(0, idx); // front(* 앞부분)
-    back = pattern.substr(idx+1); // back(* 뒷부분)
+    string front = pattern.substr(0, idx); // front(* 앞부분)
+    string back = pattern.substr(idx+1); // back(* 뒷부분)
 
     while(n--) {
         string name;
         cin >> name;
+
+        // front(* 앞부분), back(* 뒷부분)의 크기보다 name의 크기가 더 커야 함. (겹칠 수 있음.)
+        if (name.size() < (front.size() + back.size())) {
+            cout << "NE" << endl;
+            continue;
+        }
         
         int tmp = name.size()-back.size();
-        // front(* 앞부분), back(* 뒷부분)의 크기보다 name의 크기가 더 커야 함. (겹칠 수 있음.)
-        if (name.size() >= (front.size() + back.size()) && tmp > 0 && name.substr(0, idx) == front && name.substr(tmp) == back) {
+        if (tmp > 0 && name.substr(0, idx) == front && name.substr(tmp) == back) {
             cout << "DA" << endl;
         } else {
             cout << "NE" << endl;
