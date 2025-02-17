4d42ffcc079f3ce3eda6019cbaf5c3e30874c9c3
김상수 <151427064+raminkim@users.noreply.github.com>
2025-02-18 03:14:52 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+    cout << n - 543;
+
+    
+    return 0;
+}
