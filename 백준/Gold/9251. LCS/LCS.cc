b9d22b2548af7f2363315c2ea298a4b5a5aceed1
김상수 <151427064+raminkim@users.noreply.github.com>
2025-03-03 23:16:35 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5940 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.cc
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -21,18 +21,11 @@ int main() {
 
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= m; j++) {
-            // cout << a[i-1] << " " << b[j-1] << endl;
+            // a의 한글자와 b를 순차적으로 비교해가면서, 
+            // 맞으면 dp[i-1][j-1] + 1, 맞지 않으면 dp[i-1][j] or dp[i][j-1] 중 큰 값을 가져온다.
             if (a[i-1] == b[j-1]) dp[i][j]  = dp[i-1][j-1] + 1;
             else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
-
-        // for (int i = 0; i <= n; i++) {
-        //     for (int j = 0; j <= m; j++) {
-        //         cout << dp[i][j] << " ";
-        //     }
-        //     cout << endl;
-        // }
-        // cout << endl;
     }
 
     cout << dp[n][m];
