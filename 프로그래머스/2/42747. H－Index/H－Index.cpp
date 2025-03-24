b94e09553dcb4aee8a00cc7bcb70bfe954cd7c2e
김상수 <151427064+raminkim@users.noreply.github.com>
2025-03-24 22:36:32 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -12,13 +12,13 @@ int solution(vector<int> citations) {
     sort(citations.begin(), citations.end());
     int m = *max_element(citations.begin(), citations.end());
     
+    // citations의 최댓값인 m부터 while문을 돌며 검사한다.
     while (m--) {
         int cnt = 0;
         for (int i = n-1; i >= 0; i--) {
             // m 이하라면 카운트를 멈춰야 하므로, break
             if (citations[i] < m) break;
             cnt++;
-            // cout << m << " " << cnt << endl;
         }
         ans = (cnt >= m && m > ans)? m : ans;
     }
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747#) 
 
 ### 성능 요약
 
-메모리: 4.14 MB, 시간: 0.01 ms
+메모리: 3.69 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 24일 22:35:06
+2025년 03월 24일 22:36:30
 
 ### 문제 설명
 
