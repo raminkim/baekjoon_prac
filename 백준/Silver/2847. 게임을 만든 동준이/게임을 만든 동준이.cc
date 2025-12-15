2aaf84434754554a6e8a5acb3a30487b2991bb89
라민 <151427064+raminkim@users.noreply.github.com>
2025-12-16 02:06:47 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
@@ -25,7 +25,7 @@ int main() {
 
     for (int i = n-2; i >= 0; i--) {
         if (vec[i] >= vec[i+1]) {
-            int diff = (abs(vec[i+1] - vec[i]) + 1);
+            int diff = vec[i] - vec[i+1] + 1;
             vec[i] -= diff;
             result += diff;
         }
